2beba395275a5a1f3e9e867a864da33809347d8f
Kate <knaychuk@gmail.com>
2024-05-06 23:19:34 -0700

    finished rock paper scissors game

--- app/Rock-Paper-Scissors/page.jsx
+++ app/Rock-Paper-Scissors/page.jsx
@@ -1,34 +1,86 @@
 'use client'
 
+import { useState } from "react";
+import Link from 'next/navigation'
+import Image from 'next/image'
+
 const RockPaperScissors = () => {
+  const[playerScore, setPlayerScore] = useState(0);
+  const [computerScore, setComputerScore] = useState(0);
+  const[computerChoiceImage, setComputerChoiceImage] = useState('');
+  const[playerScoreColor, setPlayerScoreColor] = useState("text-black");
+  const[computerScoreColor, setComputerScoreColor] = useState("text-black");
+  const[playerScoreSize, setPlayerScoreSize] = useState("text-4xl");
+  const[computerScoreSize, setComputerScoreSize] = useState("text-4xl");
   
   const handleChoice = (playerChoice) => {
     const options = ["Rock", "Paper", "Scissors"]
     const computerChoice = Math.floor(Math.random() * options.length)
-
+    
+    setComputerChoiceImage(`/RockPaperScissors/${options[computerChoice].toLowerCase()}.png`)
+   
     console.log("Me " + playerChoice, options[playerChoice])
     console.log("Computer " + computerChoice, options[computerChoice])
    
 
     if (playerChoice === computerChoice) {
+      setPlayerScoreColor("text-red-500");
+      setComputerScoreColor("text-red-500");
+      setPlayerScoreSize("text-6xl");
+      setComputerScoreSize("text-6xl");
       console.log("Tied")
-    } else if (playerChoice === 0 && computerChoice === 2) {
-      console.log("You Win")
-    } else if (playerChoice === 1 && computerChoice === 0) {
-      console.log("You Win")
-    } else if (playerChoice === 2 && computerChoice === 1) {
+    } else if (
+    (playerChoice === 0 && computerChoice === 2) ||
+    (playerChoice === 1 && computerChoice === 0) ||
+    (playerChoice === 2 && computerChoice === 1) 
+    ) {
+      setPlayerScore((prevScore) => prevScore + 1);
+      setPlayerScoreColor("text-green-500");
+      setComputerScoreColor("text-black");
+      setPlayerScoreSize("text-6xl");
+      setComputerScoreSize("text-4xl");
       console.log("You Win")
     } else {
+      setComputerScore((prevScore) => prevScore + 1);
+      setPlayerScoreColor("text-black");
+      setComputerScoreColor("text-green-500");
+      setPlayerScoreSize("text-4xl")
+      setComputerScoreSize("text-6xl");
       console.log("You Lose")
     }
   }
 
   return (
     <div>
-      <h1>Rock, Paper, Scissors</h1>
-      <button onClick={() => handleChoice(0)}>Rock</button> 
-      <button onClick={() => handleChoice(1)}>Paper</button>
-      <button onClick={() => handleChoice(2)}>Scissors</button>
+      <div className="h-screen text-center">
+        <a href="/">
+        <Image src="/back-arrow.png" alt="" width={50} height={50} className="absolute top-5 left-5 hover:size-14 cursor-pointer "/>
+        </a>
+        <h1 className="text-4xl mt-16">Rock, Paper, Scissors</h1>
+        <h2 className="mt-2 text-gray-500">Attributions at bottom</h2>
+        <p className="text-4xl mt-12 flex flex-row justify-center items-center bg-blue-400 mx-[520px] py-10 outline-4 outline rounded-md">Computer Choice: <img className="mx-4" src={computerChoiceImage} width={100} alt="" /></p>
+        
+        <div className="mt-16">
+          <button className="mx-4 hover:scale-110" onClick={() => handleChoice(0)}><img src="/RockPaperScissors/rock.png" width={300}></img></button> 
+          <button className="mx-4 hover:scale-110" onClick={() => handleChoice(1)}><img src="/RockPaperScissors/paper.png" width={300}></img></button>
+          <button className="mx-4 hover:scale-110" onClick={() => handleChoice(2)}><img src="/RockPaperScissors/scissors.png" width={300}></img></button>
+        </div>
+        <div className="text-4xl mt-24 flex justify-center">
+          <div className={`mx-60 ${playerScoreColor} ${playerScoreSize}`}>{playerScore}</div> 
+          <div>Score</div>
+          <div className={`mx-60 ${computerScoreColor} ${computerScoreSize}`}>{computerScore}</div>
+        </div>
+      </div>
+
+      <footer className="bg-black text-white px-10 py-20">
+      <h2 className="text-xl mb-5">Attributions</h2>
+      <div className="flex flex-col">
+        <a className="mt-2 hover:underline" href="https://www.flaticon.com/free-icons/rock" title="rock icons">Rock icons created by Freepik - Flaticon</a>
+        <a className="mt-2 hover:underline" href="https://www.flaticon.com/free-icons/paper" title="paper icons">Paper icons created by Freepik - Flaticon</a>
+        <a className="mt-2 hover:underline" href="https://www.flaticon.com/free-icons/scissors" title="scissors icons">Scissors icons created by Gulraiz - Flaticon</a>
+        <a className="mt-2 hover:underline" href="https://www.flaticon.com/free-icons/arrow" title="arrow icons">Arrow icons created by Kirill Kazachek - Flaticon</a>      
+      </div>
+      </footer>
     </div>
   )
 }
--- components/ProjectCard.jsx
+++ components/ProjectCard.jsx
@@ -8,7 +8,7 @@ const ProjectCard = ({ title, img, date, url }) => {
       <Image src={img} alt="Mines Game" fill priority/>
       <div className='absolute bg-white p-5 w-full bottom-0 right-0 text-center'>
         <h1 className='text-2xl'>{title}</h1>
-        <h2 className='text-lg'>Completed: </h2>
+        <h2 className='text-lg'>Completed: {date}</h2>
       </div>
       </a>
     </div> 
