790e7e661b332acc0a2d795438e9839c904bc22a
Kate <knaychuk@gmail.com>
2024-05-07 11:09:01 -0700

    seperate project types

--- app/page.js
+++ app/page.js
@@ -1,6 +1,8 @@
 'use client'
 
-import Projects from "@/components/Projects";
+import GameProjects from "@/components/GameProjects";
+
+import TechnicalProjects from "@/components/TechnicalProjects";
 
 
 export default function Home() {
@@ -9,7 +11,16 @@ export default function Home() {
     <main className="flex flex-col items-center bg-white">
       <h1 className="mt-24 text-4xl">Boredom Projects</h1>
       <h2 className="mt-2 text-xl">By Kate Naychuk</h2>
-      <Projects/>
+
+      <div className="text-2xl mt-16">
+        <h3>Game Projects</h3>
+        <GameProjects/>
+      </div>
+
+      <div className="text-2xl mt-16">
+        <h3>Technical Projects</h3>
+        <TechnicalProjects/>
+      </div>
     </main>
   );
 }
